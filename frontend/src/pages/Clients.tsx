99f74d4918f761a794b757a3b9a02ad3f2da114d
GislainLefranc <lefranc.gislain@gmail.com>
2025-02-10 10:20:57 +0100

    mise en place de la pagination pour Clients.tsx et Users.tsx

--- frontend/src/pages/Clients.tsx
+++ frontend/src/pages/Clients.tsx
@@ -5,6 +5,7 @@ import { Client } from "../types/client.type";
 
 const Clients = () => {
   const [clients, setClients] = useState<Client[]>([]);
+  const [currentPage, setCurrentPage] = useState(1); // Page actuelle
 
   useEffect(() => {
     const fetchClients = async () => {
@@ -19,9 +20,67 @@ const Clients = () => {
     fetchClients();
   }, []);
 
+  // Calcul du nombre de pages
+  const clientsPerPage = 10;
+  const totalPages = Math.ceil(clients.length / clientsPerPage);
+
+  // Fonction pour récupérer les clients de la page actuelle
+  const currentClients = clients.slice(
+    (currentPage - 1) * clientsPerPage,
+    currentPage * clientsPerPage
+  );
+
+  // Fonction pour changer de page
+  const handleChangePage = (page: number) => {
+    if (page >= 1 || page > totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
   return (
     <>
-      <TableClients clients={clients} setClients={setClients} />
+      <TableClients clients={currentClients} setClients={setClients} />
+      {/* Boutons de pagination */}
+      <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+        <div>
+          <p className="text-sm text-gray-700">
+            De <span className="font-medium">{currentPage}</span> à{" "}
+            <span className="font-medium">{clientsPerPage}</span> sur{" "}
+            <span className="font-medium">{clients.length}</span> clients
+          </p>
+        </div>
+        <div>
+          <button
+            className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 hover:text-odaptos"
+            onClick={() => handleChangePage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Précédent
+          </button>
+
+          {[...Array(totalPages)].map((_, index) => (
+            <button
+              key={index}
+              onClick={() => handleChangePage(index + 1)}
+              className={`relative hidden items-center px-4 py-2 font-semibold ring-1 ring-gray-300 ring-inset focus:z-20 focus:outline-offset-0 md:inline-flex ${
+                currentPage === index + 1
+                  ? "bg-odaptos text-white"
+                  : "text-blue-950 hover:text-odaptos hover:bg-gray-50"
+              }`}
+            >
+              {index + 1}
+            </button>
+          ))}
+
+          <button
+            className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 hover:text-odaptos"
+            onClick={() => handleChangePage(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Suivant
+          </button>
+        </div>
+      </div>
     </>
   );
 };
--- frontend/src/pages/Users.tsx
+++ frontend/src/pages/Users.tsx
@@ -5,6 +5,7 @@ import { User } from "../types/user.type";
 
 const Users = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [currentPage, setCurrentPage] = useState(1); // Page actuelle
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -19,7 +20,69 @@ const Users = () => {
     fetchUsers();
   }, []);
 
-  return <TableUsers users={users} setUsers={setUsers} />;
+  // Calcul du nombre de pages
+  const usersPerPage = 10;
+  const totalPages = Math.ceil(users.length / usersPerPage);
+
+  // Fonction pour récupérer les utilisateurs de la page actuelle
+  const currentUsers = users.slice(
+    (currentPage - 1) * usersPerPage,
+    currentPage * usersPerPage
+  );
+
+  // Fonction pour changer de page
+  const handleChangePage = (page: number) => {
+    if (page >= 1 || page > totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
+  return (
+    <>
+      <TableUsers users={currentUsers} setUsers={setUsers} />;
+      {/* Boutons de pagination */}
+      <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+        <div>
+          <p className="text-sm text-gray-700">
+            De <span className="font-medium">{currentPage}</span> à{" "}
+            <span className="font-medium">{usersPerPage}</span> sur{" "}
+            <span className="font-medium">{users.length}</span> utilisateurs
+          </p>
+        </div>
+        <div>
+          <button
+            className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 hover:text-odaptos"
+            onClick={() => handleChangePage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Précédent
+          </button>
+
+          {[...Array(totalPages)].map((_, index) => (
+            <button
+              key={index}
+              onClick={() => handleChangePage(index + 1)}
+              className={`relative hidden items-center px-4 py-2 font-semibold ring-1 ring-gray-300 ring-inset focus:z-20 focus:outline-offset-0 md:inline-flex ${
+                currentPage === index + 1
+                  ? "bg-odaptos text-white"
+                  : "text-blue-950 hover:text-odaptos hover:bg-gray-50"
+              }`}
+            >
+              {index + 1}
+            </button>
+          ))}
+
+          <button
+            className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 hover:text-odaptos"
+            onClick={() => handleChangePage(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Suivant
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Users;
